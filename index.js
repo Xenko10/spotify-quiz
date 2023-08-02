2020d0fa65af6bd67639289edc1659cbc3bf11e9
Xenko10 <kubinios_2002@o2.pl>
2023-08-02 11:01:02 +0200

    authorization process

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 import express from "express";
 import axios from "axios";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+console.log();
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  try {
+    const access_token = req.query;
+    if (Object.keys(access_token).length === 0) {
+      res.render("index.ejs");
+    } else {
+      res.render("success.ejs", { access_token: access_token.code });
+    }
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs");
+  }
+});
+
+app.post("/get-auth", (req, res) => {
+  try {
+    let url =
+      "https://accounts.spotify.com/authorize/" +
+      "?client_id=" +
+      req.body.clientId +
+      "&response_type=code&redirect_uri=" +
+      encodeURI("http://localhost:3000/") +
+      "&show_dialog=true&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    res.redirect(url);
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs");
+  }
 });
 
 app.listen(port, () => {
