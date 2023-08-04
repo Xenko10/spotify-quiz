ae6d782b61edc51b864a579092b85a092b978718
Xenko10 <kubinios_2002@o2.pl>
2023-08-04 11:04:31 +0200

    authorization fix

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import express from "express";
+import session from "express-session";
 import axios from "axios";
-import dotenv from "dotenv";
 import bodyParser from "body-parser";
-
-dotenv.config();
-console.log();
+import queryString from "node:querystring";
 
 const app = express();
 const port = 3000;
@@ -13,34 +11,90 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  try {
-    const access_token = req.query;
-    if (Object.keys(access_token).length === 0) {
-      res.render("index.ejs");
-    } else {
-      res.render("success.ejs", { access_token: access_token.code });
-    }
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
+app.use(
+  session({
+    secret: "accessToken",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      expires: 60 * 60 * 1000,
+    },
+  })
+);
+
+app.get("/", async (req, res) => {
+  if (req.session.accessToken) {
+    res.render("success.ejs");
+  } else {
     res.render("index.ejs");
   }
 });
 
 app.post("/get-auth", (req, res) => {
   try {
+    req.session.clientId = req.body.clientId;
+    req.session.clientSecret = req.body.clientSecret;
     let url =
       "https://accounts.spotify.com/authorize/" +
       "?client_id=" +
       req.body.clientId +
       "&response_type=code&redirect_uri=" +
-      encodeURI("http://localhost:3000/") +
-      "&show_dialog=true&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+      encodeURI("http://localhost:3000/auth/") +
+      "&show_dialog=true&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private user-top-read";
     res.redirect(url);
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs");
   }
+});
+
+app.get("/auth", async (req, res) => {
+  try {
+    const credentials = `${req.session.clientId}:${req.session.clientSecret}`;
+    const encodedCredentials = Buffer.from(credentials).toString("base64");
+    const spotifyResponse = await axios.post(
+      "https://accounts.spotify.com/api/token",
+      queryString.stringify({
+        grant_type: "authorization_code",
+        code: req.query.code,
+        redirect_uri: encodeURI("http://localhost:3000/auth/"),
+      }),
+      {
+        headers: {
+          Authorization: "Basic " + encodedCredentials,
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    req.session.accessToken = spotifyResponse.data.access_token;
+    res.redirect("/success");
+  } catch (error) {
+    console.error("Failed to make request:", error);
+    res.render("index.ejs");
+  }
+});
+
+app.get("/get-artist", async (req, res) => {
+  try {
+    const result = await axios.get(
+      "https://api.spotify.com/v1/me/top/tracks?limit=50",
+      {
+        headers: {
+          Authorization: `Bearer ${req.session.accessToken}`,
+        },
+      }
+    );
+    console.log(req.session.accessToken);
+    res.render("success.ejs", { content: result.data });
+  } catch (error) {
+    console.error("Error: " + error);
+    console.log(req.session.accessToken);
+    res.render("success.ejs", { content: error });
+  }
+});
+
+app.get("/success", (req, res) => {
+  res.render("success.ejs");
 });
 
 app.listen(port, () => {
