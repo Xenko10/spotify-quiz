f01c72f8eaa5259fe1e682227cb204984b2a7e0c
Xenko10 <kubinios_2002@o2.pl>
2023-08-07 13:10:01 +0200

    making proper info after end of the game

--- public/functions.js
+++ public/functions.js
@@ -1,5 +1,19 @@
 let score = 0;
 let answersCounter = 0;
+
+const modal = document.getElementById("myModal");
+const closeButton = document.querySelector(".close");
+
+closeButton.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
 function checkAnswer(answer) {
   if (answer.getAttribute("data-correct-answer") != null) {
     document.getElementsByClassName("question")[
@@ -27,8 +41,10 @@ function checkAnswer(answer) {
   console.log(soundNumber);
   soundNumber.pause();
   answersCounter++;
+
   if (answersCounter === 10) {
-    document.getElementById("scoreDiv").innerText =
-      "Your score is " + score + " points.";
+    document.getElementById("score").innerText =
+      "Your final score is " + score + ".";
+    modal.style.display = "flex";
   }
 }
